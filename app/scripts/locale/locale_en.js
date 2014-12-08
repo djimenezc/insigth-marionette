e2b5a568bbe16f6708d7fc918584c9a567d28e3d
David Jimenez <david.calvo@clavistechnology.com>
2014-12-08 17:54:54 +0000

    EUI-1206 Create Marionette.js mock application/Example
    - Upgrade to hbs 0.10.1
    - Navigation Menu working, small issue with the style when the menu item is expanded

--- app/scripts/locale/locale_en.js
+++ app/scripts/locale/locale_en.js
@@ -5,10 +5,44 @@ define([],
 		console.log('Setting up locale labels');
 
 		return {
-			pages : {
+			pages: {
 				global: {
-					title : 'Global Scorecard'
+					title: 'Global Scorecard'
 				}
+			},
+			nav: {
+				global_dashboard: 'Global ScoreCard', // jshint ignore:line
+				'executive_dashboard': 'Executive Dashboard',// jshint ignore:line
+				'defect_detail': 'Defect Detail',// jshint ignore:line
+				'defect_summary': 'Defect Summary',// jshint ignore:line
+				'global_country_dashboard': 'Advanced Dashboard',// jshint ignore:line
+				'integrity': 'Integrity',// jshint ignore:line
+				'management_view': 'Management View',// jshint ignore:line
+				'quarterly_scorecard': 'Quarterly Scorecard',// jshint ignore:line
+				'operations_dashboard': 'Operations Dashboard',// jshint ignore:line
+				'performance_summary': 'Performance Summary',// jshint ignore:line
+				'performance_detail': 'Performance Detail',// jshint ignore:line
+				'retailer_comparison': 'Retailer Comparison',// jshint ignore:line
+				'ratings_and_reviews': 'Ratings And Reviews',// jshint ignore:line
+				'name_report': 'VOT Name',// jshint ignore:line
+				'presence_management': 'Presence Management',// jshint ignore:line
+				'portfolio_availability': 'Portfolio Availability',// jshint ignore:line
+				'summary': 'Summary',
+				'details': 'Details',
+				'products_not_listed': 'Products Not Listed',// jshint ignore:line
+				'portfolio_analysis': 'Portfolio Analysis',// jshint ignore:line
+				'price': 'Price',
+				'exceptions': 'Exceptions',
+				'performance_insights': 'Performance Insights',// jshint ignore:line
+				'search': 'Search',
+				'search_scores': 'Search Scores',// jshint ignore:line
+				'share_of_search': 'Share of Search',// jshint ignore:line
+				'menu_paths': 'Menu Placement',// jshint ignore:line
+				'geo': 'Geo',
+				'content_integrity_reports' : 'Content Integrity Reports',// jshint ignore:line
+				'imaging': 'Image Accuracy',
+				'enhanced_content': 'Enhanced Content'
 			}
+
 		};
 	});
